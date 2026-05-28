ceb0c2f7cc1759267963cb88997053c6c5111c29
Elvin Hrytsyuk <183242754+Elvin-code-dev@users.noreply.github.com>
2026-05-28 11:47:06 -0700

    Created State variables

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import { supabase } from "./supbaseClient.js";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
+
+  // this is our state varible
+  const [messages, setMessages] = useState(null);
+  const [inputvalue, setInputValue] = useState(null);
+
+
+
 	return (
 		<>
 			<p>Hello World</p>
