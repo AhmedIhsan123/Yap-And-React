79c6c197230f4ad20329cc82b9d189b2621a4708
Ahmed Ihsan <Ihsan.Ahmed@student.greenriver.edu>
2026-05-28 11:58:00 -0700

    Added a use effect to update messages and also added logic for subscribing to table changes on load

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,42 @@ import { supabase } from "./supbaseClient.js";
 import { useEffect, useState } from "react";
 
 function App() {
+	// State variables
+	const [messages, setMessages] = useState(null);
+	const [inputvalue, setInputValue] = useState(null);
 
-  // this is our state varible
-  const [messages, setMessages] = useState(null);
-  const [inputvalue, setInputValue] = useState(null);
+	// Load messages and subscribe to changes on load
+	useEffect(() => {
+		// An async method to fetch messages from the db
+		async function fetchMessagesData() {
+			const { data } = await supabase
+				.from("messages")
+				.select("*")
+				.order("created_at", { ascending: true });
+			setMessages(data);
+		}
 
+		// Make a call to fetch the data asynchronously
+		fetchMessagesData();
 
+		// sync subscription - no async needed
+		const channel = supabase
+			.channel("messages")
+			.on(
+				"postgres_changes",
+				{ event: "INSERT", schema: "public", table: "messages" },
+				(payload) => {
+					setMessages((prev) => [...prev, payload.new]);
+				},
+			)
+			.subscribe();
+
+		return () => supabase.removeChannel(channel);
+	}, []);
 
 	return (
 		<>
-			<p>Hello World</p>
+			<p>Messages: {messages}</p>
 		</>
 	);
 }
